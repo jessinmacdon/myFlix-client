add7dfcad76abe699da7962054b7b2f4e340eadb
jessinmacdon <jessinmacdon@gmail.com>
2022-03-17 02:15:02 +0100

    code and folders updates

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,70 +1,75 @@
 import React from 'react';
 
+import axios from 'axios';
+
 //import ReactDOM from 'react-dom';
 
-import MovieCard from '../movie-card/movie-card';
+import { LoginView } from '../login-view/login-view';
+
+import { MovieCard } from '../movie-card/movie-card';
 
 import { MovieView } from '../movie-view/movie-view';
 
-class MainVeiw extends React.Component {
+export class MainView extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            movies: [
-                {
-                    _id: 1,
-                    Title: 'Inception',
-                    Description: 'Dom Cobb is a skilled thief, the absolute best in the dangerous art of extraction, stealing valuable secrets from deep within the subconscious during the dream state, when the mind is at its most vulnerable. Cobb\'s rare ability has made him a coveted player in this treacherous new world of corporate espionage, but it has also made him an international fugitive and cost him everything he has ever loved.',
-                    Genre: 'Action/Adventure/Sci-Fi',
-                    Director: 'Christopher Nolan',
-                    ImagePath: '.....'
-                },
-
-                {
-                    _id: 2,
-                    Title: 'Transformers',
-                    Description: 'High-school student Sam Witwicky buys his first car, who is actually the Autobot Bumblebee. Bumblebee defends Sam and his girlfriend Mikaela Banes from the Decepticon Barricade, before the other Autobots arrive on Earth',
-                    Genre: 'Action/adventure/Sci-Fi',
-                    Director: 'Michael Bay',
-                    ImagePath: '......'
-                },
-
-                {
-                    _id: 3,
-                    Title: 'Gladiator',
-                    Description: 'Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius. Before his death, the Emperor chooses Maximus to be his heir over his own son, Commodus, and a power struggle leaves Maximus and his family condemned to death.',
-                    Genre: 'Action/Adventure/Drama',
-                    Director: 'Ridley Scott',
-                    ImagePath: '.....'
-                }
-            ],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            user: null
         };
     }
 
+    componentDidMount() {
+        axios.get('https://macdon-myflix.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    //updates selected movie state/property to show that particular movie
     setSelectedMovie(newSelectedMovie) {
         this.setState({
             selectedMovie: newSelectedMovie
         });
     }
 
+    //signUp - user resgistration
+    /*onRegistration(registration) {
+        this.setState({
+            registration,
+        });
+    }*/
+
+    //if loggin is succesful, this function updates the user property in state to that of the particular user.
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
+
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
         if (movies.length === 0)
-            return <div className="main-view">The list is empty!</div>;
+            return <div className="main-view" //>The list is empty!</div
+            />;
 
         return (
             <div className="main-view">
                 {selectedMovie ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                    : movies.map((movie) => (
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
                     ))}
             </div>
         );
     }
 }
-
-
-export default MainVeiw;
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class MovieCard extends React.Component {
+export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
         return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
     }
 }
 
-export default MovieCard;
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImageURL: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        })
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
