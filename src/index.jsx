8d6e330fc7b3d5653de4f0bdc5dc6f563e15d7cf
jessinmacdon <jessinmacdon@gmail.com>
2022-03-17 10:30:07 +0100

    code fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
+import { MainView } from './components/main-view/main-view';
 
 //import statement showing we need bundle index.scss
 import './index.scss';
 
 //Main component
-class myFlixApplication extends React.Component {
+class MyFlixApplication extends React.Component {
     render() {
         return (
             <MainView />
@@ -18,4 +18,4 @@ class myFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 //React renders app in root DOM element
-ReactDOM.render(React.createElement(myFlixApplication), container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);
