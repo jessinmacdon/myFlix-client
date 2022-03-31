ef25c583a4ab47c02078b4c52306aecf946d06fa
jessinmacdon <jessinmacdon@gmail.com>
2022-03-31 13:28:19 +0200

    Update movieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 import '../movie-card/movie-card.scss';
 
 export class MovieCard extends React.Component {
@@ -24,7 +25,18 @@ MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string.isRequired,
         Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birthyear: PropTypes.string,
+            Deathyear: PropTypes.string
+        }),
+        Featured: PropTypes.bool,
         ImagePath: PropTypes.string.isRequired
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    onBackClick: PropTypes.func
 };
